9ddf79678b8080b51f245e1a938dc6c925175cd8
Emanuel Araujo <emanuel.qa@gmail.com>
2018-12-17 22:43:49 -0300

    Start

---
+++ src/main/java/com/emanuel/cursocm/CursocmApplication.java
@@ -0,0 +1,14 @@
+package com.emanuel.cursocm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursocmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursocmApplication.class, args);
+	}
+
+}
+
