e9ce4d32f698e910977cc370ed9971c6bc888b62
Emanuel <emanuel.qa@gmail.com>
2019-06-30 21:14:53 -0300

    Endpoint /cliente/{id} disponivel

---
+++ src/main/java/com/emanuel/cursocm/services/ClienteService.java
@@ -0,0 +1,21 @@
+package com.emanuel.cursocm.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.emanuel.cursocm.domain.Cliente;
+import com.emanuel.cursocm.repositories.ClienteRepository;
+
+@Service
+public class ClienteService {
+
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
+	public Cliente buscar(Integer id) {
+		Optional<Cliente> cliente = clienteRepository.findById(id);
+		return cliente.orElse(null);
+	}
+}
