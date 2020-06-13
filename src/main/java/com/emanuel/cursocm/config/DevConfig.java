534052af13bd1d6c72c8b7d71859bc9fe59ad3d3
Emanuel <emanuel.qa@gmail.com>
2020-06-12 22:17:36 -0300

    64-Implementando SmtpEmailServicecom servidor do Google

--- src/main/java/com/emanuel/cursocm/config/DevConfig.java
+++ src/main/java/com/emanuel/cursocm/config/DevConfig.java
@@ -9,6 +9,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import com.emanuel.cursocm.services.DBService;
+import com.emanuel.cursocm.services.EmailService;
+import com.emanuel.cursocm.services.SmtpEmailService;
 
 @Configuration
 @Profile("dev")
@@ -27,6 +29,11 @@ public class DevConfig {
 		}
 		dbService.instantiateTestDataBase();
 		return true;	
+	}
+	
+	@Bean
+	public EmailService emailService() {
+		return new SmtpEmailService();
 	}
 
 }
