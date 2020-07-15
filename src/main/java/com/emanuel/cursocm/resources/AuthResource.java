8ef58731a6a0f99f0b218aa4029561f22409ec4d
Emanuel <emanuel.qa@gmail.com>
2020-07-14 21:15:28 -0300

    77 - Refresh token

---
+++ src/main/java/com/emanuel/cursocm/resources/AuthResource.java
@@ -0,0 +1,30 @@
+package com.emanuel.cursocm.resources;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.emanuel.cursocm.security.JWTUtil;
+import com.emanuel.cursocm.security.UserSpringSecurity;
+import com.emanuel.cursocm.services.UserService;
+
+@RestController
+@RequestMapping(value = "/auth")
+public class AuthResource {
+	
+	@Autowired
+	private JWTUtil jwtUtil;
+	
+	@PostMapping(value = "/refresh_token")
+	public ResponseEntity<Void> refreshToken(HttpServletResponse response){
+		UserSpringSecurity user = UserService.authenticated();
+		String token = jwtUtil.generationToken(user.getUsername());
+		response.addHeader("Authorization", "Bearer " + token);
+		return ResponseEntity.noContent().build();
+	}
+
+}
