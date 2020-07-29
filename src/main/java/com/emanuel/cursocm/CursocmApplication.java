8ebdd9b901f476ac590700537da4333b7890c5fd
Emanuel <emanuel.qa@gmail.com>
2020-07-28 22:09:05 -0300

    84 - Salvando primeiro arquivo no S3

--- src/main/java/com/emanuel/cursocm/CursocmApplication.java
+++ src/main/java/com/emanuel/cursocm/CursocmApplication.java
@@ -1,11 +1,17 @@
 package com.emanuel.cursocm;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.emanuel.cursocm.services.S3Service;
+
 @SpringBootApplication
 public class CursocmApplication implements CommandLineRunner {
+	
+	@Autowired
+	private S3Service s3Service;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursocmApplication.class, args);
@@ -13,7 +19,7 @@ public class CursocmApplication implements CommandLineRunner {
 	
 	@Override
 	public void run(String... args) throws Exception {
-		
+		s3Service.uploadFile("C:\\Users\\Emanuel\\Downloads\\logo.png");
 	}
 
 }
